b1b16ba00d5799ce1ccfbaba0279496bc4f17368
Dani Marie <mariedwilliams2@gmail.com>
2020-02-04 20:34:44 -0600

    Texas Flag

---
+++ js/external.js
@@ -0,0 +1,25 @@
+alert ("Welcome to my Website!");
+
+var userInput = prompt('What is your favorite color?');
+console.log('The user entered:' + White);
+alert("Great! My favorite color is White, too.");
+
+//first problem
+var userInput = prompt('How much are nine Disney movies at $3 a movie?');
+console.log(typeOf userPrompt);'var price = (3 * 9); price; 27;
+alert("Yes, 27 is he answer.");
+
+
+
+//second problem
+var userInput = prompt('What is the sum of my 3 paychecks?');
+console.log(typeOf userPrompt);' var payment =  (3 * 1130); payment;
+alert("Yes, the answer is $3,390.");
+
+
+
+
+//third problem
+var userInput = prompt('How much are nine Disney movies at $3 a movie?');
+console.log(typeOf userPrompt);' var price = (3 *9); price; 27;
+alert("Yes, the answer is $3,390.");
