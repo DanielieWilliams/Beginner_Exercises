5f9c5e186e31418dc2a91f1f41010b7451d4ddf3
Dani Marie <mariedwilliams2@gmail.com>
2020-02-07 19:49:27 -0600

    Studying-Reviewing

---
+++ Studying/studying.js
@@ -0,0 +1,16 @@
+"user strict;"
+
+function isTrue(input){
+    return input === true;
+}
+// console.log (isTrue([1,2]));
+
+function isFalse(value){
+    return value === false;
+}
+// console.log (isFalse(false));
+
+function not(anyInput){
+    return !anyInput
+}
+// console.log(not(false));
