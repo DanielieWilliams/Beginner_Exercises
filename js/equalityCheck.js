0b24509c09e8559c7b740a0ff1947487decc7bf3
Dani Marie <mariedwilliams2@gmail.com>
2020-02-07 19:11:25 -0600

    Edabit Challenges

---
+++ js/equalityCheck.js
@@ -0,0 +1,31 @@
+"user strict;"
+
+
+// In this challenge, you must verify the equality of two different
+// given parameters: a and b.
+//
+//     Both the value and the type of parameters need to be
+//     tested in order to have an matching equality. The possible types of the given parameters are:
+//
+// Numbers
+// Strings
+// Booleans (false or true)
+// Special values: undefined, null and NaN
+//
+// What have you learnt so far that will permit you to do two different checks (value and type) with a single statement?
+//
+//     Implement a function that returns true if the parameters are equal, and false if they are different.
+
+function checkEquality(isPassword, userInput) {
+    return isPassword === userInput;
+}
+// console.log(checkEquality("a", "b"));
+
+// Create a function that takes two numbers as arguments
+// and return their sum.
+
+function addition(a,b){
+    return a+b;
+}
+
+// console.log (addition("I and Love ", "And You"));
