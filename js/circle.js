73dbfd6282c21bccd7d41bc3f2194c31b79642bc
Dani Marie <mariedwilliams2@gmail.com>
2020-02-18 15:28:22 -0600

    Math-Circles-BOM

---
+++ js/circle.js
@@ -0,0 +1,129 @@
+(function () {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+        getArea: function () {
+
+
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            function getArea(radius) {
+
+                getArea(3);
+
+
+                // TODO: return the proper value
+                return Math.PI * (radius ^ 2);
+
+                // logInfo: function (doRounding) {
+                // TODO: complete this method.
+
+                // If doRounding is true, round the result to the nearest integer.
+                // Otherwise, output the complete value
+                function getrandomInt(doRounding) {
+                    return (Math.floor(Math.random() * (max - min) + min + 2));
+                }
+
+
+                }
+
+                console.log(Math.round(4));
+                console.log("Area of a circle with radius:" + this.radius + ", is: ");
+
+
+                // log info about the circle
+                console.log("Raw circle information");
+                circle.logInfo(false);
+                console.log("Circle information rounded to the nearest whole number");
+                circle.logInfo(true);
+
+                console.log("=======================================================");
+                // TODO: Change the radius of the circle to 5.
+
+                // log info about the circle
+                console.log("Raw circle information", c.area().toFixed(3));
+                circle.logInfo(false);
+                console.log("Circle information rounded to the nearest whole number", 'perimeter = ' , c.perimeter().toFixed(3);
+                circle.logInfo(true);
+
+        })();
+
+
+
+
+var circle = {
+    radius: 3,
+};
+
+
+console.log("Area of a circle with radius:" + this.radius + ", is: ");
+circle.forEach(
+
+
+
+
+
+
+////////////NOTES//////////////
+////math.random, math.floor, math.ceiling, math.how, math.sqaureroot methods/////
+
+
+// Returns a random integer between min and max.
+//
+// function getRandomInt(min, max)
+// {
+
+var max = 10;
+var min = 1;
+console.log(Math.ceil(Math.random() * (max - min) + min - 1));
+// }
+
+
+//hit minimum when you put your -1 "minimum after the second min//
+
+
+var max = 10;
+var min = 1;
+console.log(Math.round(Math.random() * (max - min) + min));
+
+console.log(Math.round(20.49));  // 20
+Math.round(20.5);   // 21
+Math.round(-20.5);  // -20
+Math.round(-20.51); // -21
+//////
+
+// Returns a random integer between min and max.
+// function getRandomInt(min, max) {
+
+var max = 30;
+var min = 25;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+// }
+
+////
+
+// 2 raised to the 16 power
+//we can use sqrt in pow//
+console.log(Math.sqrt(Math.pow(4, 3))); // 65536
+//cube root function will get us the cubic root//
+
+
+console.log(Math.sqrt(parseFloat("99 luftballoons")));  // NaN
+console.log(Math.sqrt(true));  // 1
+
+
+////PI//// ////FUNCTION FOR MATH.PI /////
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+}
+
+console.log(Math.PI);
+console.log(calculateCircumference(10));
+Math.sqrt(0);  // 0
+Math.sqrt(-1); // NaN
+
+
+}
