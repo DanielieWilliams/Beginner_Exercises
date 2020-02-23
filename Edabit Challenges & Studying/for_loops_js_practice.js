327b62f623355c456850c834c3b6bb1eefac58bf
Dani Marie <mariedwilliams2@gmail.com>
2020-02-23 13:27:54 -0600

    Studying PractPractice All Day

--- Edabit Challenges & Studying/for_loops_js_practice.js
+++ Edabit Challenges & Studying/for_loops_js_practice.js
@@ -17,7 +17,64 @@
 // For Loop -> initializes -> checks the condition -> if its true it goes to the body --> increment. ---> if false, then done.
 
 function showMultiplicationTable(number){
-    for (var i = 1; i < 10; i++)
+    for (var i = 1; i < 11;  i++)
     console.log('7 x ' +i + " =", + number * i);
 }showMultiplicationTable(7);
+
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+//
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+// ...
+
+for (var i = 20; i <= 29; i++) {
+    var randomNumber = Math.floor(Math.random() * 201) + 10;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even.");
+    } else {
+        console.log(randomNumber + " is odd.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
