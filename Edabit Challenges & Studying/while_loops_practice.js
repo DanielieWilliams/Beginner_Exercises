f1afe39f41f8ebda8f604c21edfc982870329b65
Dani Marie <mariedwilliams2@gmail.com>
2020-02-23 15:49:35 -0600

    Practice All Day

--- Edabit Challenges & Studying/while_loops_practice.js
+++ Edabit Challenges & Studying/while_loops_practice.js
@@ -36,3 +36,59 @@ while (i < 10) {
 }
 /// while loop iteration #0
 // while loop iteration #1 and so on//
+
+
+// 3. An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers.
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log(allCones);
+do {
+
+    var conesBought = Math.floor(Math.random() * 5) + 1;
+    console.log(conesBought);
+
+        if (conesBought > allCones) {
+            console.log("I don't have enough");
+
+        } else {
+            allCones -= conesBought;
+            console.log("I have " + allCones + "cones left.");
+            }
+        }
+        while (allCones > 0);
+        console.log("I've sold out. Please come back tomorrow.");
+
+
+
+
+// //Algorithm//
+// randomNumber 50 - 100 = conesToSell
+// generate 1-5 = conesBought
+// console.log = amt of cones sold
+
+
+// ///DO WHILE EXAMPLE///
+//         var i = 99;
+//
+//         do {
+//             console.log('while loop iteration #' + i);
+//             i++;
+//         } while (i < 99);
+//     }
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
